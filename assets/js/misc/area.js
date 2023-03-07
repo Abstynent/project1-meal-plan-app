f5bcf40d41b2bc07bb7f2d082cb263a66927e6ba
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-03-07 20:27:51 +0000

    set API URLs as const, save misc files to present object that can be used --to be removed later

---
+++ assets/js/misc/area.js
@@ -0,0 +1,85 @@
+{
+    "meals": [
+        {
+            "strArea": "American"
+        },
+        {
+            "strArea": "British"
+        },
+        {
+            "strArea": "Canadian"
+        },
+        {
+            "strArea": "Chinese"
+        },
+        {
+            "strArea": "Croatian"
+        },
+        {
+            "strArea": "Dutch"
+        },
+        {
+            "strArea": "Egyptian"
+        },
+        {
+            "strArea": "French"
+        },
+        {
+            "strArea": "Greek"
+        },
+        {
+            "strArea": "Indian"
+        },
+        {
+            "strArea": "Irish"
+        },
+        {
+            "strArea": "Italian"
+        },
+        {
+            "strArea": "Jamaican"
+        },
+        {
+            "strArea": "Japanese"
+        },
+        {
+            "strArea": "Kenyan"
+        },
+        {
+            "strArea": "Malaysian"
+        },
+        {
+            "strArea": "Mexican"
+        },
+        {
+            "strArea": "Moroccan"
+        },
+        {
+            "strArea": "Polish"
+        },
+        {
+            "strArea": "Portuguese"
+        },
+        {
+            "strArea": "Russian"
+        },
+        {
+            "strArea": "Spanish"
+        },
+        {
+            "strArea": "Thai"
+        },
+        {
+            "strArea": "Tunisian"
+        },
+        {
+            "strArea": "Turkish"
+        },
+        {
+            "strArea": "Unknown"
+        },
+        {
+            "strArea": "Vietnamese"
+        }
+    ]
+}
