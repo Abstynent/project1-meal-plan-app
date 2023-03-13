c6565f6453a3062a4ebd04ab74b02ff7a8274725
kristiyan <kristiyan.tefov@icloud.com>
2023-03-13 22:41:02 +0000

    navigation buttons added and date and time added

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,3 +234,38 @@ $(function() {
         };
     }
 });
+
+$(function() {
+    if(window.location.search == "?meal") {
+        let backNav = document.getElementById("navbar")
+        let backBtn = document.createElement("div")
+        backBtn.innerHTML = `<a href="./search.html?meal"><img class="positionbackbtn" src="./assets/images/left-arrow.png" /></a>`
+        backNav.prepend(backBtn) 
+    }
+    else if(window.location.search == "?cocktail") {
+        let backNav = document.getElementById("navbar")
+        let backBtn = document.createElement("div")
+        backBtn.innerHTML = `<a href="./search.html?cocktail"><img class="positionbackbtn" src="./assets/images/left-arrow.png" /></a>`
+        backNav.prepend(backBtn)
+    }
+});
+
+$(function() {
+    if(window.location.search == "?meal" || "?cocktail") {
+        let backNav = document.getElementById("navbar")
+        let homeBtn = document.createElement("div")
+        homeBtn.innerHTML = `<a href="./index.html"><img class="positionbackbtn" src="./assets/images/home.png" /></a>`
+        backNav.append(homeBtn)
+       
+    }
+});
+
+
+async function setTime() {
+    let currentTime = dayjs();
+    $("#currentDay").text(currentTime.format("MMM D YYYY"));
+    $("#currentTime").text(currentTime.format("HH:mm:ss"));
+
+  };
+  setTime();
+  setInterval(setTime, 1000);
